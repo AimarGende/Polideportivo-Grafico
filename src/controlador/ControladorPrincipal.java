5e6648738b3424579790a05d45ea0b5fa699c10a
Aimar <aimargende@gmail.com>
2023-03-06 14:09:22 +0100

    arreglado

--- src/controlador/ControladorPrincipal.java
+++ src/controlador/ControladorPrincipal.java
@@ -1,5 +1,37 @@
 package controlador;
 
-public class ControladorPrincipal {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import vista.*;
+
+public class ControladorPrincipal implements ActionListener{
+	private Principal principal;
+
+	public ControladorPrincipal(Principal principal) {
+		this.principal = principal;
+		
+		principal.ActividadBTN.addActionListener(this);
+		principal.UsuariosBTN.addActionListener(this);
+		principal.InscripcionBTN.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()== principal.ActividadBTN) {
+			ActividadFormulario form = new ActividadFormulario();
+			
+			form.setVisible(true);
+		}
+		
+		if(e.getSource()== principal.UsuariosBTN) {
+			
+		}
+		
+		if(e.getSource()== principal.InscripcionBTN) {
+			
+		}
+		
+	}
+	
 }
--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -1,18 +1,19 @@
 package vista;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.EventQueue;
+
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
-
+	public JButton UsuariosBTN ;
+	public JButton ActividadBTN;
+	public JButton InscripcionBTN;
 	/**
 	 * Launch the application.
 	 */
@@ -41,20 +42,16 @@ public class Principal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Usuarios");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(159, 44, 137, 23);
-		contentPane.add(btnNewButton);
+		UsuariosBTN = new JButton("Usuarios");
+		UsuariosBTN.setBounds(159, 44, 137, 23);
+		contentPane.add(UsuariosBTN);
 		
-		JButton btnNewButton_1 = new JButton("Actividad");
-		btnNewButton_1.setBounds(159, 106, 137, 23);
-		contentPane.add(btnNewButton_1);
+		ActividadBTN = new JButton("Actividad");
+		ActividadBTN.setBounds(159, 106, 137, 23);
+		contentPane.add(ActividadBTN);
 		
-		JButton btnNewButton_2 = new JButton("Inscripcion");
-		btnNewButton_2.setBounds(159, 165, 137, 23);
-		contentPane.add(btnNewButton_2);
+		InscripcionBTN = new JButton("Inscripcion");
+		InscripcionBTN.setBounds(159, 165, 137, 23);
+		contentPane.add(InscripcionBTN);
 	}
 }
