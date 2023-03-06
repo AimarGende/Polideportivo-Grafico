ab74a1df2db396af3cecf66b2643f8fd44479807
Aimar <aimargende@gmail.com>
2023-03-06 13:48:31 +0100

    First commit

--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Principal extends JFrame {
 
@@ -36,6 +39,22 @@ public class Principal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Usuarios");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(159, 44, 137, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Actividad");
+		btnNewButton_1.setBounds(159, 106, 137, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Inscripcion");
+		btnNewButton_2.setBounds(159, 165, 137, 23);
+		contentPane.add(btnNewButton_2);
 	}
-
 }
